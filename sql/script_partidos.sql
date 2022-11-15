c7db8eb986cbebc4b0095c28a9c647d02c363938
Erick Mejia <68398454+erickm13@users.noreply.github.com>
2022-11-15 14:40:54 -0600

    Add files via upload

---
+++ sql/script_partidos.sql
@@ -0,0 +1,59 @@
+INSERT INTO quinielas.partidos (fecha_hora,estadio,id_equipo1,id_equipo2,fase_de_partido) VALUES ('2022-11-21 19:00','al_bayt',1,2,'Grupos');
+INSERT INTO quinielas.partidos (fecha_hora,estadio,id_equipo1,id_equipo2,fase_de_partido) VALUES ('2022-11-21 13:00','al_thumama',3,4,'Grupos');
+INSERT INTO quinielas.partidos (fecha_hora,estadio,id_equipo1,id_equipo2,fase_de_partido) VALUES ('2022-11-21 16:00','khalifa',5,6,'Grupos');
+INSERT INTO quinielas.partidos (fecha_hora,estadio,id_equipo1,id_equipo2,fase_de_partido) VALUES ('2022-11-21 22:00','ahmed_bin_ali',7,8,'Grupos');
+
+INSERT INTO quinielas.partidos (fecha_hora,estadio,id_equipo1,id_equipo2,fase_de_partido) VALUES ('2022-11-22 22:00','al_janoub',13,14,'Grupos');
+INSERT INTO quinielas.partidos (fecha_hora,estadio,id_equipo1,id_equipo2,fase_de_partido) VALUES ('2022-11-22 16:00','educacion',15,16,'Grupos');
+INSERT INTO quinielas.partidos (fecha_hora,estadio,id_equipo1,id_equipo2,fase_de_partido) VALUES ('2022-11-22 19:00','974',11,12,'Grupos');
+INSERT INTO quinielas.partidos (fecha_hora,estadio,id_equipo1,id_equipo2,fase_de_partido) VALUES ('2022-11-22 13:00','lusail',9,10,'Grupos');
+
+INSERT INTO quinielas.partidos (fecha_hora,estadio,id_equipo1,id_equipo2,fase_de_partido) VALUES ('2022-11-23 22:00','ahmed_bin_ali',21,22,'Grupos');
+INSERT INTO quinielas.partidos (fecha_hora,estadio,id_equipo1,id_equipo2,fase_de_partido) VALUES ('2022-11-23 19:00','al_thumama',17,18,'Grupos');
+INSERT INTO quinielas.partidos (fecha_hora,estadio,id_equipo1,id_equipo2,fase_de_partido) VALUES ('2022-11-23 16:00','khalifa',19,20,'Grupos');
+INSERT INTO quinielas.partidos (fecha_hora,estadio,id_equipo1,id_equipo2,fase_de_partido) VALUES ('2022-11-23 13:00','al_bayt',23,24,'Grupos');
+
+INSERT INTO quinielas.partidos (fecha_hora,estadio,id_equipo1,id_equipo2,fase_de_partido) VALUES ('2022-11-24 13:00','al_janoub',27,28,'Grupos');
+INSERT INTO quinielas.partidos (fecha_hora,estadio,id_equipo1,id_equipo2,fase_de_partido) VALUES ('2022-11-24 16:00','educacion',31,32,'Grupos');
+INSERT INTO quinielas.partidos (fecha_hora,estadio,id_equipo1,id_equipo2,fase_de_partido) VALUES ('2022-11-24 19:00','974',29,30,'Grupos');
+INSERT INTO quinielas.partidos (fecha_hora,estadio,id_equipo1,id_equipo2,fase_de_partido) VALUES ('2022-11-24 22:00','lusail',25,26,'Grupos');
+
+INSERT INTO quinielas.partidos (fecha_hora,estadio,id_equipo1,id_equipo2,fase_de_partido) VALUES ('2022-11-25 13:00','ahmed_bin_ali',8,6,'Grupos');
+INSERT INTO quinielas.partidos (fecha_hora,estadio,id_equipo1,id_equipo2,fase_de_partido) VALUES ('2022-11-25 16:00','al_thumama',1,3,'Grupos');
+INSERT INTO quinielas.partidos (fecha_hora,estadio,id_equipo1,id_equipo2,fase_de_partido) VALUES ('2022-11-25 19:00','khalifa',4,2,'Grupos');
+INSERT INTO quinielas.partidos (fecha_hora,estadio,id_equipo1,id_equipo2,fase_de_partido) VALUES ('2022-11-25 22:00','al_bayt',5,7,'Grupos');
+
+INSERT INTO quinielas.partidos (fecha_hora,estadio,id_equipo1,id_equipo2,fase_de_partido) VALUES ('2022-11-26 13:00','al_janoub',16,14,'Grupos');
+INSERT INTO quinielas.partidos (fecha_hora,estadio,id_equipo1,id_equipo2,fase_de_partido) VALUES ('2022-11-26 16:00','educacion',12,10,'Grupos');
+INSERT INTO quinielas.partidos (fecha_hora,estadio,id_equipo1,id_equipo2,fase_de_partido) VALUES ('2022-11-26 19:00','974',13,15,'Grupos');
+INSERT INTO quinielas.partidos (fecha_hora,estadio,id_equipo1,id_equipo2,fase_de_partido) VALUES ('2022-11-26 22:00','lusail',9,11,'Grupos');
+
+INSERT INTO quinielas.partidos (fecha_hora,estadio,id_equipo1,id_equipo2,fase_de_partido) VALUES ('2022-11-27 13:00','ahmed_bin_ali',20,18,'Grupos');
+INSERT INTO quinielas.partidos (fecha_hora,estadio,id_equipo1,id_equipo2,fase_de_partido) VALUES ('2022-11-27 16:00','al_thumama',21,23,'Grupos');
+INSERT INTO quinielas.partidos (fecha_hora,estadio,id_equipo1,id_equipo2,fase_de_partido) VALUES ('2022-11-27 19:00','khalifa',24,22,'Grupos');
+INSERT INTO quinielas.partidos (fecha_hora,estadio,id_equipo1,id_equipo2,fase_de_partido) VALUES ('2022-11-27 22:00','al_bayt',17,19,'Grupos');
+
+INSERT INTO quinielas.partidos (fecha_hora,estadio,id_equipo1,id_equipo2,fase_de_partido) VALUES ('2022-11-28 13:00','al_janoub',28,26,'Grupos');
+INSERT INTO quinielas.partidos (fecha_hora,estadio,id_equipo1,id_equipo2,fase_de_partido) VALUES ('2022-11-28 16:00','educacion',32,30,'Grupos');
+INSERT INTO quinielas.partidos (fecha_hora,estadio,id_equipo1,id_equipo2,fase_de_partido) VALUES ('2022-11-28 19:00','974',25,27,'Grupos');
+INSERT INTO quinielas.partidos (fecha_hora,estadio,id_equipo1,id_equipo2,fase_de_partido) VALUES ('2022-11-28 22:00','lusail',29,31,'Grupos');
+
+INSERT INTO quinielas.partidos (fecha_hora,estadio,id_equipo1,id_equipo2,fase_de_partido) VALUES ('2022-11-29 22:00','ahmed_bin_ali',8,5,'Grupos');
+INSERT INTO quinielas.partidos (fecha_hora,estadio,id_equipo1,id_equipo2,fase_de_partido) VALUES ('2022-11-29 22:00','al_thumama',6,7,'Grupos');
+INSERT INTO quinielas.partidos (fecha_hora,estadio,id_equipo1,id_equipo2,fase_de_partido) VALUES ('2022-11-29 18:00','khalifa',2,3,'Grupos');
+INSERT INTO quinielas.partidos (fecha_hora,estadio,id_equipo1,id_equipo2,fase_de_partido) VALUES ('2022-11-29 18:00','al_bayt',4,1,'Grupos');
+
+INSERT INTO quinielas.partidos (fecha_hora,estadio,id_equipo1,id_equipo2,fase_de_partido) VALUES ('2022-11-30 18:00','al_janoub',14,15,'Grupos');
+INSERT INTO quinielas.partidos (fecha_hora,estadio,id_equipo1,id_equipo2,fase_de_partido) VALUES ('2022-11-30 18:00','educacion',16,13,'Grupos');
+INSERT INTO quinielas.partidos (fecha_hora,estadio,id_equipo1,id_equipo2,fase_de_partido) VALUES ('2022-11-30 22:00','974',12,9,'Grupos');
+INSERT INTO quinielas.partidos (fecha_hora,estadio,id_equipo1,id_equipo2,fase_de_partido) VALUES ('2022-11-30 22:00','lusail',10,11,'Grupos');
+
+INSERT INTO quinielas.partidos (fecha_hora,estadio,id_equipo1,id_equipo2,fase_de_partido) VALUES ('2022-12-01 18:00','ahmed_bin_ali',24,21,'Grupos');
+INSERT INTO quinielas.partidos (fecha_hora,estadio,id_equipo1,id_equipo2,fase_de_partido) VALUES ('2022-12-01 18:00','al_thumama',22,23,'Grupos');
+INSERT INTO quinielas.partidos (fecha_hora,estadio,id_equipo1,id_equipo2,fase_de_partido) VALUES ('2022-12-01 22:00','khalifa',20,17,'Grupos');
+INSERT INTO quinielas.partidos (fecha_hora,estadio,id_equipo1,id_equipo2,fase_de_partido) VALUES ('2022-12-01 22:00','al_bayt',18,19,'Grupos');
+
+INSERT INTO quinielas.partidos (fecha_hora,estadio,id_equipo1,id_equipo2,fase_de_partido) VALUES ('2022-12-02 18:00','al_janoub',30,31,'Grupos');
+INSERT INTO quinielas.partidos (fecha_hora,estadio,id_equipo1,id_equipo2,fase_de_partido) VALUES ('2022-12-02 18:00','educacion',32,29,'Grupos');
+INSERT INTO quinielas.partidos (fecha_hora,estadio,id_equipo1,id_equipo2,fase_de_partido) VALUES ('2022-12-02 22:00','974',26,27,'Grupos');
+INSERT INTO quinielas.partidos (fecha_hora,estadio,id_equipo1,id_equipo2,fase_de_partido) VALUES ('2022-12-02 22:00','lusail',28,25,'Grupos');
